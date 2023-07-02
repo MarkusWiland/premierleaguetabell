70a2a7a0aa06d706e40140b65885e57ec341943b
Markus Wiland <markus.wiland@outlook.com>
2023-07-02 20:10:18 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require("path");
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "scss")],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
